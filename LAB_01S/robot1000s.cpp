2375340f26f7930a5453a8d82f43e49740635e3b
Shayennn <me@shayennn.com>
2019-12-09 21:53:20 +0700

    Add comment

--- LAB_01S/robot1000s.cpp
+++ LAB_01S/robot1000s.cpp
@@ -8,13 +8,19 @@ int main(){
     int K, cmd_count[4];  // NSEW
     int first_reduce=0, second_reduce=2;
     bool best_choice = true;
+
     cin >> cmd;
     cin >> K;
-    if(K>100)K=100;
+
+    if(K>100)K=100;  // Filter too much K
+
+    // Init count
     cmd_count[0] = 0;
     cmd_count[1] = 0;
     cmd_count[2] = 0;
     cmd_count[3] = 0;
+
+    // Count cmd
     for(int i=0; i<100; i++){
         if(cmd[i] == '\0')break;
         switch (cmd[i])
@@ -33,17 +39,32 @@ int main(){
             break;
         }
     }
+
+    // Choose cmd to reduce
     if(cmd_count[0] > cmd_count[1])first_reduce=1;
     if(cmd_count[2] > cmd_count[3])second_reduce=3;
-    for(int i=0;i<K;i++){
-        if(cmd_count[first_reduce]>0)cmd_count[first_reduce]--;
-        else if(cmd_count[second_reduce]>0)cmd_count[second_reduce]--;
+
+    // Reduce cmd
+    while(K!=0){
+
+        // Reduce y axis
+        if(cmd_count[first_reduce]>0){
+            cmd_count[first_reduce]--;
+            K--;
+        }
+        // Reduce x axis
+        else if(cmd_count[second_reduce]>0){
+            cmd_count[second_reduce]--;
+            K--;
+        }
+        // Switch mode to reduce other command.
         else if(best_choice){
             first_reduce = (first_reduce == 1)?0:1;
             second_reduce = (second_reduce == 2)?3:2;
             best_choice = false;
-            i--;
         }
+        // If no cmd to reduce exit loop
+        if(cmd_count[0]+cmd_count[1]+cmd_count[2]+cmd_count[3] == 0)break;
     }
     cout << abs(cmd_count[0]-cmd_count[1]+cmd_count[2]-cmd_count[3])*2 << endl;
 }
