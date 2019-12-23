b91650b62836a66d64263322bfa22e0a57ecc544
Shayennn <me@shayennn.com>
2019-12-23 16:23:07 +0700

    Finished listwork2

---
+++ LAB_03S/listwork2.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+typedef int valueType;
+
+struct ListNode
+{
+  valueType val;
+  ListNode* next;
+
+  ListNode(valueType val, ListNode* next=0)
+    : val(val), next(next) {}
+};
+
+void insert_front(ListNode* header, valueType x)
+{
+    ListNode* old_second = header->next;
+    header->next = new ListNode(x);
+    header->next->next = old_second;
+}
+
+void append_list(ListNode* header, valueType x)
+{
+    ListNode* p = header;
+    while (p->next != 0)
+    {
+        p = p->next;
+    }
+    p->next = new ListNode(x);
+    // p->next->next = 0;
+}
+
+void free_list(ListNode*& header)
+{
+    ListNode* tmp = header->next;
+    ListNode* tmp2;
+    while(tmp->next != 0){
+        tmp2 = tmp;
+        tmp = tmp->next;
+        delete tmp2;
+    }
+}
+
+void delete_list(ListNode*& header, int index){
+    index--;
+    ListNode* p = header;
+    ListNode* q;
+    if(p == 0) return;
+    for(int i=0; i<index;i++){
+        if(p->next != 0)p = p->next;
+        else return;
+    }
+    // if(p->next == 0)return;
+    if(p->next == 0){
+        q = 0;
+    }else{
+        q = p->next->next;
+    }
+    delete p->next;
+    p->next = q;
+}
+
+int main()
+{
+    ListNode* header = new ListNode(0);
+    header->next = 0;
+    int M, X;
+    char cmd;
+    cin >> M;
+    for(int i = 0; i<M; i++){
+        cin >> cmd >> X;
+        if(cmd == 'I'){
+            insert_front(header, X);
+        }else if(cmd == 'A')
+        {
+            append_list(header, X);
+        }else
+        {
+            delete_list(header, X);
+        }
+        
+    }
+    while(header->next != 0){
+        cout << header->next->val << endl;
+        header = header->next;
+    }
+}
