2b67d832050006594a9cb9f5114f43d9dd2ae142
Shayennn <me@shayennn.com>
2019-12-25 00:26:39 +0700

    Adjust to wrost case

--- LAB_03S/fuckyou_trains.py
+++ LAB_03S/fuckyou_trains.py
@@ -1,14 +1,17 @@
 import random
 
-n = 10
+n = 100000
+
+lon = [_ for _ in range(1, 1000001)]
+random.shuffle(lon)
 
 f = open('test_trains.txt', 'w')
 f.write(str(n))
 f.write('\n')
 for i in range(n):
-    if random.random() > 0.2:
-        f.write('N ' + str(i+1) + ' '+str(random.randint(1, 10))+'\n')
+    if random.random() > 0:
+        f.write('N ' + str(lon[i]) + ' '+str(random.randint(1, 100000))+'\n')
     else:
-        f.write('M ' + str(random.randint(1, 10)) +
-                ' '+str(random.randint(1, 10))+'\n')
+        f.write('M ' + str(random.randint(0, 100000)) +
+                ' '+str(random.randint(0, 100000))+'\n')
 f.close()
