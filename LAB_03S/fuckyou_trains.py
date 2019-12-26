cc9b86c4703cb10ba0f6b3409ef7f7377610d6dc
Shayennn <me@shayennn.com>
2019-12-26 23:07:53 +0700

    More normaly random

--- LAB_03S/fuckyou_trains.py
+++ LAB_03S/fuckyou_trains.py
@@ -9,7 +9,7 @@ f = open('test_trains.txt', 'w')
 f.write(str(n))
 f.write('\n')
 for i in range(n):
-    if random.random() > 0:
+    if random.random() > 0.2:
         f.write('N ' + str(lon[i]) + ' '+str(random.randint(1, 100000))+'\n')
     else:
         f.write('M ' + str(random.randint(0, 100000)) +
